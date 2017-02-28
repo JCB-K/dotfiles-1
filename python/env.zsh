47e9ef1105f5c5dc34c6454fa05c3c2bf710363f
JCB-K <hello@jcbk.me>
2017-02-28 17:34:23 +0100

    Remove unused stuff and add some apps

--- python/env.zsh
+++ python/env.zsh
@@ -1,11 +1,11 @@
-# =======================================================================
-# Python/Virtualenv
-# =======================================================================
+# # =======================================================================
+# # Python/Virtualenv
+# # =======================================================================
 
-export WORKON_HOME=$HOME/.virtualenvs
-export PROJECT_HOME=$HOME/src
-export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python
-export VIRTUALENVWRAPPER_VIRTUALENV=/usr/local/bin/virtualenv
-export VIRTUALENVWRAPPER_VIRTUALENV_ARGS='--no-site-packages'
+# export WORKON_HOME=$HOME/.virtualenvs
+# export PROJECT_HOME=$HOME/src
+# export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python
+# export VIRTUALENVWRAPPER_VIRTUALENV=/usr/local/bin/virtualenv
+# export VIRTUALENVWRAPPER_VIRTUALENV_ARGS='--no-site-packages'
 
-source /usr/local/bin/virtualenvwrapper.sh
+# source /usr/local/bin/virtualenvwrapper.sh
--- zsh/config.zsh
+++ zsh/config.zsh
@@ -46,7 +46,7 @@ source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 zstyle ':completion:*' hosts off
 setopt extended_glob
-ZSH=$HOME/.oh-my-zsh
+export ZSH=/Users/jacob/.oh-my-zsh
 ZSH_THEME="robbyrussell"
 plugins=(git osx)
 source $ZSH/oh-my-zsh.sh
